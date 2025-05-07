ec9b7e41303864b2f204793144ada8023d362570
Jo Sungchan <teacher076@bssm.hs.kr>
2025-05-07 20:21:06 +0900

    1

--- test1.py
+++ test1.py
@@ -14,6 +14,6 @@ col1.subheader("i am column1 subheader")
 col2.checkbox("this i checkbox2 in col2")
 
 with tab1 :
-        st.write('hello')
+        st.write('hello111111111111111111ㄴㄴ1111111111111111111111111111111111111111111111111111111111111111111111')
 with tab2 :
         st.write("hi")
