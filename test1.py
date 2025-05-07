3222f38b51c4f4e1e6c41e48e807484eec847e88
Jo Sungchan <teacher076@bssm.hs.kr>
2025-05-07 20:20:20 +0900

    위치 변경

--- test1.py
+++ test1.py
@@ -4,10 +4,7 @@ col1, col2 = st.columns([2, 3])
 tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
 with col1 :
     st.title("here is column1 title")
-    with tab1 :
-        st.write('hello')
-    with tab2 :
-        st.write("hi")
+    
 
 with col2 :
     st.title("here is column2 title")
@@ -15,3 +12,8 @@ with col2 :
 
 col1.subheader("i am column1 subheader")
 col2.checkbox("this i checkbox2 in col2")
+
+with tab1 :
+        st.write('hello')
+with tab2 :
+        st.write("hi")
